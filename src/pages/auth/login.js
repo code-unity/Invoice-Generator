7f2d021fcd7b86800056be6d3b5fc1af2537ea6f
SaiSri Angajala <angajalasaisri2000@gmail.com>
2022-07-30 11:16:26 +0530

    Add logo

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,41 +1,36 @@
-import React, { useState} from 'react';
-import Card from '@material-ui/core/Card';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Backdrop from '@material-ui/core/Backdrop';
-import history from '../../history';
-import axios from 'axios';
-import './login.css';
+import React, { useState } from "react";
+import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Backdrop from "@material-ui/core/Backdrop";
+import history from "../../history";
+import axios from "axios";
+import "./login.css";
+import logo from "../../logo.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& .MuiTextField-root': {
+    "& .MuiTextField-root": {
       margin: theme.spacing(1),
-      width: '35ch',
+      width: "35ch",
     },
-
   },
   card: {
     maxWidth: 345,
-    margin:'auto',
-    marginTop:"100px"
+    margin: "auto",
+    marginTop: "100px",
   },
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
+    color: "#fff",
   },
 }));
 
-
-
-  
-
-
 function LoginForm() {
   const classes = useStyles();
-  const [details, setDetails] = useState({ username: '', password: '' });
+  const [details, setDetails] = useState({ username: "", password: "" });
   const [open, setOpen] = React.useState(false);
   // const [error, setError] = useState('');
   // const [error1, setError1] = useState('');
@@ -43,91 +38,112 @@ function LoginForm() {
   const submitHandler = (e) => {
     e.preventDefault();
     setOpen(!open);
-    const data ={
+    const data = {
       username: details.username,
-      password: details.password
-    }
+      password: details.password,
+    };
 
-    axios.post(`${process.env.REACT_APP_API_URL}/admin`,data,{ headers: { 'Content-Type': 'application/json' } })
-    .then(res=>{
-      setOpen(false);
-      localStorage.setItem('token',res.data.token);
-      localStorage.setItem('signedin',res.data.status.success);
-      history.push('/home');
-    })
-    .catch(err=>{
-      setOpen(false);
-    })
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/admin`, data, {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((res) => {
+        setOpen(false);
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("signedin", res.data.status.success);
+        history.push("/home");
+      })
+      .catch((err) => {
+        setOpen(false);
+      });
   };
 
   return (
     <div>
       <Card className={classes.card}>
         <div>
-            <div>
-              <div className="logo">
+          <div>
+            <div className="logo">
               <img
-                src="https://www.codeunity.in/images/logo/logo.png"
+                src={logo}
                 loading="lazy"
                 alt="CodeUnity logo"
                 id="ember499"
                 style={{
-                  marginLeft: '31%',
-                  marginTop: '10%',
-                  marginBottom:'10px',
-                  height: '0%',
-                  width: '40%',
+                  marginLeft: "31%",
+                  marginTop: "10%",
+                  marginBottom: "10px",
+                  height: "0%",
+                  width: "40%",
                 }}
               ></img>
-              </div>
-              <h2 style={{marginLeft:'83px',marginTop:'10px',fontSize:'16px',marginBottom:'-5px'}}>Log In to Your Account</h2>
-              <form className={classes.root} onSubmit={submitHandler}>
-                <div style={{marginLeft:"10px"}}>
-                  <TextField
-                    required
-                    id="outlined-full-width"
-                    label="Username"
-                    variant="outlined"
-                    inputProps={{
-                      className:classes.textField
-                    }}
-                    onChange={(e) => {
-                      setDetails({ ...details, username: e.target.value });
-                    }}
-                  />
-                  {/* {error1 !== '' ? (
+            </div>
+            <h2
+              style={{
+                marginLeft: "83px",
+                marginTop: "10px",
+                fontSize: "16px",
+                marginBottom: "-5px",
+              }}
+            >
+              Log In to Your Account
+            </h2>
+            <form className={classes.root} onSubmit={submitHandler}>
+              <div style={{ marginLeft: "10px" }}>
+                <TextField
+                  required
+                  id="outlined-full-width"
+                  label="Username"
+                  variant="outlined"
+                  inputProps={{
+                    className: classes.textField,
+                  }}
+                  onChange={(e) => {
+                    setDetails({ ...details, username: e.target.value });
+                  }}
+                />
+                {/* {error1 !== '' ? (
                   <div className="error"> &nbsp; &nbsp; &nbsp; {error1}</div>
                   ) : (
                   ''
                   )} */}
-                  <TextField
-                    id="outlined-password-input"
-                    label="Password"
-                    type="password"
-                    autoComplete="current-password"
-                    variant="outlined"
-                    fullWidth
-                    onChange={(e) =>
-                      setDetails({ ...details, password: e.target.value })
-                    }
-                  />
-                  {/* {error !== '' ? (
+                <TextField
+                  id="outlined-password-input"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                  variant="outlined"
+                  fullWidth
+                  onChange={(e) =>
+                    setDetails({ ...details, password: e.target.value })
+                  }
+                />
+                {/* {error !== '' ? (
                   <div className="error"> &nbsp; &nbsp; &nbsp; {error}</div>
                     ) : (
                       ''
                     )} */}
-                    
-                </div>
-                <div style={{marginLeft:"20px",marginBottom:"20px",marginTop:'5px'}}>
-                    <Button variant="contained" color="primary" onClick={submitHandler}>
-                      Log In
-                    </Button>
-                    <Backdrop className={classes.backdrop} open={open} >
-                      <CircularProgress color="primary" />
-                    </Backdrop>
-                </div>
-              </form>         
-            </div>
+              </div>
+              <div
+                style={{
+                  marginLeft: "20px",
+                  marginBottom: "20px",
+                  marginTop: "5px",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={submitHandler}
+                >
+                  Log In
+                </Button>
+                <Backdrop className={classes.backdrop} open={open}>
+                  <CircularProgress color="primary" />
+                </Backdrop>
+              </div>
+            </form>
+          </div>
         </div>
       </Card>
     </div>
